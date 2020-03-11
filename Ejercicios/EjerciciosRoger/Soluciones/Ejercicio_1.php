5c57b95d2af53654bb19bab5b1c17614f2b3d973
Camilo Manuel Couselo Alonso <manuel.couseloalonso@gmail.com>
2020-03-11 13:51:30 +0100

    ejercicio 1

--- Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_1.php
+++ Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_1.php
@@ -15,26 +15,26 @@ NOTA: Ejecuta el codigo como un script y no através de un servidor
 */
 
 
+
 function calcula_descuento($precio_compra)
 {
     if ($precio_compra < 800) {
         echo "0% Descuento - Total: " . $precio_compra;
-    } elseif (800 <= $precio_compra && $precio_compra >= 1500 && $precio_compra < 5000) {
+    } elseif (800 >= $precio_compra or 1500 >= $precio_compra) { //Entra en el bucle
         $desc = $precio_compra * 0.01;
         $total = $precio_compra - $desc;
         echo "10% desc. - Total: " . $total;
-    } elseif (1500 < $precio_compra && $precio_compra <= 5000) {
-        $desc = $precio_compra * 0.015;
-        $total = $precio_compra - $desc;
-        echo "15% desc. - Total: " . $total;
-    } elseif (5000 < $precio_compra) {
-        $desc = $precio_compra * 0.02;
-        $total = $precio_compra - $desc;
-        echo "20% desc. - Total: " . $total;
+    } elseif (1500 < $precio_compra && $precio_compra <= 5000) { //Entra
+        $desc1 = $precio_compra * 0.015;
+        $total1 = $precio_compra - $desc1;
+        echo "15% desc. - Total: " . $total1;
+    } elseif (5000 < $precio_compra) { //Entra
+        $desc2 = $precio_compra * 0.02;
+        $total2 = $precio_compra - $desc2;
+        echo "20% desc. - Total: " . $total2;
     } else {
         echo "Error";
     }
-
 }
 
-calcula_descuento(800);
+calcula_descuento(9000);
