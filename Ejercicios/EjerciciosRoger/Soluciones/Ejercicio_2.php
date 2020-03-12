4c44e343dd5502b40cbfd30fd295e39497ded384
Camilo Manuel Couselo Alonso <manuel.couseloalonso@gmail.com>
2020-03-12 10:52:03 +0100

    reformat code

--- Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_2.php
+++ Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_2.php
@@ -3,28 +3,49 @@
 function Heron($A, $B, $C)
 {
     $semiperimetro = ($A + $B + $C) / 2;
+
     $area = sqrt($semiperimetro * ($semiperimetro - $A) * ($semiperimetro - $B) * ($semiperimetro - $C));
+
     return $area;
 }
 
 // Determinar triangulos
 
-function tipo_triangulo($ladoA, $ladoB, $ladoC)
+function tipoTriangulo($ladoA, $ladoB, $ladoC)
 {
     if ($ladoA == $ladoB and $ladoB == $ladoC) { //Todos los lados iguales
+
         $areaEq = Heron($ladoA, $ladoB, $ladoC);
+
         echo "Triangulo Equilatero y su area es " . $areaEq;
+
+        return $areaEq;
+
     } elseif ($ladoA != $ladoB and $ladoB != $ladoC and $ladoA != $ladoC) { //Todos los lados distintos
+
         $areaEs = Heron($ladoA, $ladoB, $ladoC);
-        echo "Triangulo Escaleno y su area es " . $areaEs;
-    } else { //Dos lados iguales y uno distinto
+
+        echo "Triangulo Escaleno y su area es ";
+
+        return $areaEs;
+
+    } elseif ($ladoA = $ladoB or $ladoB = $ladoC) { //Dos lados iguales y uno distinto
+
         $areaIs = Heron($ladoA, $ladoB, $ladoC);
-        echo "Triangulo Isosceles y su area es " . $areaIs;
+
+        echo "Triangulo Isosceles y su area es ";
+
+        return $areaIs;
+
+    } else {
+
+        return "error";
+
     }
 
 }
 
-echo tipo_triangulo(4, 4, 4);
+echo tipoTriangulo(5, 4, 5);
 
 /* CORRECCION 1
 
