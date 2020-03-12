d2b10543f04adcc1098bbf6ffbb24ba94e4ab50a
Camilo Manuel Couselo Alonso <manuel.couseloalonso@gmail.com>
2020-03-12 18:52:30 +0100

    fixture

--- Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_5.php
+++ Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_5.php
@@ -3,35 +3,40 @@
 function arbolito($numero)
 {
     if (0 < $numero) {
+
+        if (0 < $numero) {
 //pintamos el escalon del 1 a N
-        for ($i = 1; $i <= $numero; $i++) {
+            for ($i = 1; $i <= $numero; $i++) {
 
-            echo $i . " \n";
+                echo "\n";
 
-            for ($j = 1; $j <= $i; $j++) {
+                for ($j = 1; $j <= $i; $j++) {
 
-                echo $j . " ";
+                    echo $j . " ";
+                }
             }
-        }
 
 //Pinta de N a 1
-        for ($i = $numero - 1; $i >= 1; $i--) {
+            for ($i = $numero - 1; $i >= 1; $i--) {
 
-            echo $i . " \n";
+                echo "\n";
 
-            for ($j = 1; $j <= $i; $j++) {
+                for ($j = 1; $j <= $i; $j++) {
 
-                echo $j . " ";
+                    echo $j . " ";
 
+                }
             }
-        }
 
+        }
     }
 
 }
 
 
-print_r(arbolito(6));
+echo arbolito(6);
+
+
 
 /*
 CORRECCION 1:
