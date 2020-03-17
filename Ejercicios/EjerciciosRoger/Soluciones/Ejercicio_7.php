c82159a6f09f0250ad3ec8b8230e69da1f14fe22
okuda <manuel.couseloalonso@gmail.com>
2020-03-17 01:53:26 +0100

    need resolve array to string conversion

--- Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_7.php
+++ Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_7.php
@@ -1,11 +1,58 @@
 <?php
 $array = array(
-    "key0" => "A",
-    "key1" => "B",
-    "key2" => "C",
-    "key3" => ["key1" => ["key11" => "valor11", "llave" => "value", "otro" => ["10" => 1, "10000" => 2]]]);
+    'key0' => 'A',
+    'key1' => 'B',
+    'key2' => 'C',
+    'key3' => [
+        'key1' => [
+            'key11' => 'valor11',
+            'llave' => 'value',
+            'otro' => [
+                '10' => 1,
+                '10000' => 2]
+        ]
+    ]
+);
 
-print_r(array_values($array));
+echo "Nivel 0.\n";
+
+foreach ($array as $item_1 => $value_1) {
+
+    echo sprintf('%s => %s%s', $item_1, $value_1, PHP_EOL);
+
+    if (is_array($value_1)) {
+
+        echo "Nivel 1.\n";
+
+        foreach ($value_1 as $item_2 => $value_2) {
+
+            echo sprintf('%s => %s%s', $item_2, $value_2, PHP_EOL);
+
+        }
+
+        echo "Nivel 2.\n";
+
+        if (is_array($value_2)) {
+
+            foreach ($value_2 as $item_3 => $value_3) {
+
+                echo sprintf('%s => %s%s', $item_3, $value_3, PHP_EOL);
+            }
+        }
+
+        echo "Nivel 3.\n";
+
+        if (is_array($value_3)) {
+
+            foreach ($value_3 as $item_4 => $value_4) {
+
+                echo sprintf('%s => %s%s', $item_4, $value_4, PHP_EOL);
+
+            }
+
+        }
+    }
+}
 
 /*
 CORRECCION 1:
