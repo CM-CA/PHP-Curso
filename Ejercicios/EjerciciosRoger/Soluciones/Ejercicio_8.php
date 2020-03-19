c98754aa4a50045b96df1cf4a28ea59187371c8b
Camilo Manuel Couselo Alonso <manuel.couseloalonso@gmail.com>
2020-03-19 11:48:42 +0100

    new try

--- Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_8.php
+++ Ejercicios/EjerciciosRoger/Soluciones/Ejercicio_8.php
@@ -2,34 +2,54 @@
 function calcularPar($numero)
 {
     if ($numero % 2 == 0) {
+
         return true;
+
     } else {
+
         return false;
+
     }
 }
-
 function comprobarNumero($numero)
 {
     if (is_int($numero)) {
+
         $arrayNumeros = str_split($numero);
-        foreach ($arrayNumeros as $key => $item) {
-            if ($key >= 4) {
-               return false;
-            }
-        }
-        for ($i = 0; $i < count($arrayNumeros); $i++) {
-            $numeroArray = $arrayNumeros[$i];
-            $finalComprobacion = calcularPar($numeroArray);
-            if ($finalComprobacion === false) {
+
+        /* foreach ($arrayNumeros as $key => $item) {
+
+             if ($key >= 4) {
+
                 return false;
+
+             }
+
+         }*/
+
+        for ($i = 0; $i < count($arrayNumeros); $i++) {
+
+            $numeroArray = $arrayNumeros[$i];
+
+            $finalComprobacion = calcularPar($numeroArray);
+
+            if ($finalComprobacion === false) {
+
+                return false;
+
                 break;
+
             }
         }
 
     } else {
+
         return 'El numero debe ser entero';
+
     }
+
     return true;
+
 }
 
 function esVerdadero($numero)
@@ -42,5 +62,5 @@ function esVerdadero($numero)
 }
 
 
-echo esVerdadero(2224);
+echo esVerdadero(2222222222222222222222222222222222222222222222222222);
 
